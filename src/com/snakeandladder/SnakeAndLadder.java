a4cc21abdeaff4901446590b4dc9cca0582873c5
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 19:47:12 +0530

    Player Checks th e option no play, ladder or snake as in UC3

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,9 @@ package com.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
+	public static final int NO_PLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE =3;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Problem");
@@ -13,9 +16,20 @@ public class SnakeAndLadder {
 		
 		Random random = new Random();
 		int dieNum = random.nextInt(7-1)+1;
+		System.out.println("Dice number : "+dieNum);
 		
-
+		int choice = random.nextInt(4-1)+1;
 		
+		if(choice == NO_PLAY){
+			System.out.println("No Play");
+		}else if(choice == LADDER) {
+			System.out.println("Player inclined by "+dieNum);
+			position+=dieNum;
+		}else if(choice == SNAKE) {
+			System.out.println("Player declined by "+dieNum);
+			position-=dieNum;
+		}
+			
 	}
 
 }
